f546a744ec3838b58d113c6fd847fe1ea79324d3
dexsoft <dexsoft@proton.me>
2025-10-09 17:37:25 +0800

    fixed bugs

--- transport/grpc/grpc.go
+++ transport/grpc/grpc.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"crypto/tls"
 	"net"
-	"time"
 
 	"go-micro.dev/v5/cmd"
 	"go-micro.dev/v5/transport"
@@ -14,7 +13,6 @@ import (
 	mls "go-micro.dev/v5/util/tls"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/keepalive"
 
 	pb "go-micro.dev/v5/transport/grpc/proto"
 )
@@ -103,11 +101,6 @@ func (t *grpcTransport) Dial(addr string, opts ...transport.DialOption) (transpo
 
 	options := []grpc.DialOption{
 		grpc.WithTimeout(dopts.Timeout),
-		grpc.WithKeepaliveParams(keepalive.ClientParameters{
-			Time:                30 * time.Second, // send pings every 30 seconds if there is no activity
-			Timeout:             10 * time.Second, // wait 10 seconds for ping ack before considering the connection dead
-			PermitWithoutStream: true,             // send pings even without active streams
-		}),
 	}
 
 	if t.opts.Secure || t.opts.TLSConfig != nil {
--- transport/http_transport.go
+++ transport/http_transport.go
@@ -5,7 +5,6 @@ import (
 	"crypto/tls"
 	"net"
 	"net/http"
-	"time"
 
 	"go-micro.dev/v5/logger"
 	maddr "go-micro.dev/v5/util/addr"
@@ -63,17 +62,11 @@ func (h *httpTransport) Dial(addr string, opts ...DialOption) (Client, error) {
 		config.NextProtos = []string{"http/1.1"}
 
 		conn, err = newConn(func(addr string) (net.Conn, error) {
-			return tls.DialWithDialer(&net.Dialer{
-				Timeout:   dopts.Timeout,
-				KeepAlive: 30 * time.Second,
-			}, "tcp", addr, config)
+			return tls.DialWithDialer(&net.Dialer{Timeout: dopts.Timeout}, "tcp", addr, config)
 		})(addr)
 	} else {
 		conn, err = newConn(func(addr string) (net.Conn, error) {
-			return (&net.Dialer{
-				Timeout:   dopts.Timeout,
-				KeepAlive: 30 * time.Second,
-			}).Dial("tcp", addr)
+			return net.DialTimeout("tcp", addr, dopts.Timeout)
 		})(addr)
 	}
 
